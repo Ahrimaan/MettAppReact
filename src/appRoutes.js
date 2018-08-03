5ebd69f9e29f213448e07a1c52d8f89ea5643b34
Pawel Warmuth <ahrimaan@Pawels-MacBook-Pro.local>
2018-08-03 19:51:55 +0200

    added tenant router

--- src/appRoutes.js
+++ src/appRoutes.js
@@ -1,7 +1,8 @@
 import { AppointmentRoutes } from './appointment';
 import { AdminRoutes  } from './admin';
 import { loginRoutes } from './login';
+import { TenantRoutes } from './tenant';
 
 export default [
-    AppointmentRoutes, AdminRoutes, loginRoutes
+    AppointmentRoutes, AdminRoutes, loginRoutes, TenantRoutes
 ];
