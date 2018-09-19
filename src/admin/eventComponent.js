6d3795c67737552e3f4921470b7273fbdbfb70f5
Pawel Warmuth <ahrimaan@Pawels-MBP.fritz.box>
2018-09-19 19:46:28 +0200

    added showing all ateendes and sum

--- src/admin/eventComponent.js
+++ src/admin/eventComponent.js
@@ -4,10 +4,14 @@ import { Table, Header, Icon, Grid } from 'semantic-ui-react';
 
 class AdminEventComponent extends Component {
     render() {
+        let { id } = this.props.match.params;
+        let { events } = this.props.appointment;
+        let users = events.find(item => item.id === id).attendees;
         return (
             <Grid>
                 <Grid.Row columns={3}>
                     <Grid.Column>
+
                     </Grid.Column>
                     <Grid.Column>
                         <Table basic='very' celled collapsing>
@@ -19,62 +23,41 @@ class AdminEventComponent extends Component {
                             </Table.Header>
 
                             <Table.Body>
-                                <Table.Row>
-                                    <Table.Cell>
-                                        <Header as='h4' image>
-                                            <Icon name="user" />
-                                            <Header.Content>
-                                                Lena
-                      <Header.Subheader>Human Resources</Header.Subheader>
-                                            </Header.Content>
-                                        </Header>
-                                    </Table.Cell>
-                                    <Table.Cell>22</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                    <Table.Cell>
-                                        <Header as='h4' image>
-                                            <Icon name="user" />
-                                            <Header.Content>
-                                                Matthew
-                      <Header.Subheader>Fabric Design</Header.Subheader>
-                                            </Header.Content>
-                                        </Header>
-                                    </Table.Cell>
-                                    <Table.Cell>15</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                    <Table.Cell>
-                                        <Header as='h4' image>
-                                            <Icon name="user" />
-                                            <Header.Content>
-                                                Lindsay
-                      <Header.Subheader>Entertainment</Header.Subheader>
-                                            </Header.Content>
-                                        </Header>
-                                    </Table.Cell>
-                                    <Table.Cell>12</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                    <Table.Cell>
-                                        <Header as='h4' image>
-                                            <Icon name="user" />
-                                            <Header.Content>
-                                                Mark
-                      <Header.Subheader>Executive</Header.Subheader>
-                                            </Header.Content>
-                                        </Header>
-                                    </Table.Cell>
-                                    <Table.Cell>11</Table.Cell>
-                                </Table.Row>
+                                {this.getuserTable(users)}
                             </Table.Body>
                         </Table>
+                        <div> To Buy : <b>{users.reduce((cur, val) => cur + val.value, 0)} </b> </div>
                     </Grid.Column>
                 </Grid.Row>
             </Grid >
 
         )
     }
+
+    getuserTable(users) {
+        if (users) {
+            return users.map(user => {
+                return (
+                    <Table.Row key={user.email}>
+                        <Table.Cell>
+                            <Header as='h4' image>
+                                <Icon name="user" />
+                                <Header.Content>
+                                    {user.username ? user.username : user.email}
+                                </Header.Content>
+                            </Header>
+                        </Table.Cell>
+                        <Table.Cell> {user.value} </Table.Cell>
+                    </Table.Row>
+                )
+            })
+        }
+
+    }
 }
 
-export default connect(null, null)(AdminEventComponent)
+function mapStateToProps(props) {
+    return { appointment: props.appointment };
+}
+
+export default connect(mapStateToProps, null)(AdminEventComponent)
